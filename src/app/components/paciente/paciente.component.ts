ca4dd9c18b3751f25b059e7431ec349fe2e7ecda
greverom <greverom4@gmail.com>
2024-05-19 10:32:19 -0500

    mod-pacientes

--- src/app/components/paciente/paciente.component.ts
+++ src/app/components/paciente/paciente.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { PacienteModel } from '../paciente.model';
 import { NgForm } from '@angular/forms';
 import { PacienteService } from '../../service/paciente.service';
-import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -15,7 +14,8 @@ export class PacienteComponent implements OnInit{
 paciente:PacienteModel = new PacienteModel();
 
 constructor(private servicio : PacienteService,
-            private parametro : ActivatedRoute){}
+            private parametro : ActivatedRoute,
+            private ruta : Router){}
 
 ngOnInit(): void {
     let id = this.parametro.snapshot.paramMap.get('id');
@@ -34,37 +34,44 @@ guardar( form: NgForm ){
     return;
   }
 
-   let peticion : Observable<any>;
    if(this.paciente.id){
     
-    peticion = this.servicio.refreshPaciente(this.paciente);
-   
+     this.servicio.refreshPaciente(this.paciente)
+     .subscribe( () => {
+    
+      Swal.fire({
+      title: 'Actualizado!!',  
+      text: `Agregado los cambios de ${this.paciente.nombres}`,
+      icon: 'success',
+      timer: 1800,
+      showConfirmButton: false
+      })
+      setTimeout(()=>{
+        this.ruta.navigate(['pacientes']);
+      },1500)
+ });
   }else{
     
-    peticion = this.servicio.crearPaciente(this.paciente);
-   
-  }
-   
-    peticion.subscribe( data =>{
-    console.log(data)
-
+      this.servicio.crearPaciente(this.paciente)
+      .subscribe( () => {
+    
         Swal.fire({
-        title: 'Exito',  
-        text: `${this.paciente.nombres} se ha guardado exitosamente!!`,
+        title: 'Guardado!!',  
+        text: `${this.paciente.nombres} se ha agregado `,
         icon: 'success',
         timer: 1800,
         showConfirmButton: false
         })
    });
-  
+  }
+   
+
 }
 
 limpiar(form:NgForm){
   this.paciente.id.delete
   return form.reset();
 }
-
-
 
 
 }
